2718e2d06df46792317b8ff9da7b3ca84b390ec7
as01 <adityasharan2001@gmail.com>
2023-07-23 21:21:12 +0530

    completed the search feature

--- script.js
+++ script.js
@@ -1,59 +1,55 @@
-const apiKey = "86f98f9a";
+var itemsPerPage = 10; // Number of items to display per page
+var currentPage = 1; // Current page number
+var apiKey = "86f98f9a";
+var searchQueryInput = document.getElementById("searchQueryInput");
+var searchQuery = 'Avatar';
+var dataArray = fetchMovieData();
 
 // Function to fetch movie data from the OMDB API
-function fetchMovieData(searchQuery) {
-    const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchQuery}`;
-    const dataReceived = [];
+function fetchMovieData() {
+    var apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchQuery}`;
+  dataArray = [];
 
-    for (var i = 1; i <= 5; ++i) {
-        // Make the GET request using the Fetch API
-        fetch(apiUrl + `&page=${i}`)
-            .then((response) => {
-                // Check if the response status is OK (200)
-                if (!response.ok) {
-                    throw new Error("Network response was not ok");
-                }
-                // Parse the JSON data from the response
-                return response.json();
-            })
-            .then((data) => {
-                // Process the data here
-                dataReceived.push(data);
-                console.log(data);
-            })
-            .catch((error) => {
-                // Handle any errors that occurred during the fetch
-                console.error("Fetch error:", error);
-            });
-    }
-    return dataReceived;
+  var fetchPromises = [];
+  for (var i = 1; i <= 5; ++i) {
+    fetchPromises.push(fetch(apiUrl + `&page=${i}`).then(response => response.json()));
+  }
+
+  // Wait for all fetch requests to complete using Promise.all()
+  Promise.all(fetchPromises)
+    .then(results => {
+      // 'results' is an array containing the resolved data from all fetch requests
+      dataArray = results;
+      // Now that we have fetched the data, we can display the movie cards for the first page
+      currentPage = 1;
+      displayCurrentPageData();
+    })
+    .catch(error => {
+      console.error("Error occurred while fetching movie data:", error);
+    });
 }
 
-// Call the function to fetch movie data
-const dataArray = fetchMovieData("Avengers");
 
-const itemsPerPage = 10; // Number of items to display per page
-let currentPage = 1; // Current page number
 
 // Function to create a movie card element
 function createMovieCard(movie) {
     // Create a div element for the movie card
-    const card = document.createElement("div");
+    var card = document.createElement("div");
     card.classList.add("movie-card");
 
     // Create an image element for the movie poster
-    const posterImg = document.createElement("img");
+    var posterImg = document.createElement("img");
     posterImg.src = movie.Poster;
     posterImg.alt = movie.Title;
     posterImg.classList.add("poster");
 
     // Add an event listener to handle image loading errors
-    posterImg.onerror = function () {
+    posterImg.error = function () {
         posterImg.src = "placeholder.jpg"; // Replace with your placeholder image URL
     };
 
     // Create a paragraph element for the movie title
-    const titlePara = document.createElement("p");
+    var titlePara = document.createElement("p");
     titlePara.textContent = movie.Title;
     titlePara.classList.add("title");
 
@@ -67,19 +63,21 @@ function createMovieCard(movie) {
 
 // Function to display movie cards for the current page
 function displayCurrentPageData() {
-    const movieContainer = document.getElementById("movieContainer");
+    var movieContainer = document.getElementById("movieContainer");
     movieContainer.innerHTML = ""; // Clear the container
+    if (!dataArray || !dataArray[currentPage - 1] || !dataArray[currentPage - 1]["Search"]) {
+        // Handle invalid page number or missing 'Search' property
+        console.error("No movie data found for the current page.");
+        return;
+    }
 
-    console.log(dataArray);
-    const currentPageData = dataArray[currentPage - 1]["Search"];
+    var currentPageData = dataArray[currentPage - 1];
+    var currentPageSearchData = currentPageData["Search"];
 
-    if (!currentPageData) return; // Handle invalid page number
-
-    for (let i = 0; i < itemsPerPage; i++) {
-        const movie = currentPageData[i];
+    for (var i = 0; i < itemsPerPage; i++) {
+        var movie = currentPageSearchData[i];
         if (movie) {
-            const movieCard = createMovieCard(movie);
-            console.log(movieCard);
+            var movieCard = createMovieCard(movie);
             movieContainer.appendChild(movieCard);
         }
     }
@@ -88,12 +86,13 @@ function displayCurrentPageData() {
 // Function to handle page navigation when a pagination button is clicked
 function changePage(pageNumber) {
     currentPage = pageNumber;
-    displayCurrentPageData();
+    displayCurrentPageData(pageNumber);
 }
 
-var searchQuery = 'Avatar';
-
-// Call the function to fetch movie data and display the first page of data
-fetchMovieData(searchQuery);
-displayCurrentPageData();
-changePage(1);
+// Function to handle the search when the Search button is clicked
+function searchMovies() {
+    searchQuery = searchQueryInput.value;
+    if(searchQuery.length == 0) return;
+    // Call the fetchMovieData function with the new searchQuery
+    fetchMovieData();
+}
